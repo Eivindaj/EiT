28b24c9175c34eab33345d8db8d66f31e5eb88d8
Eivindaj <eivind339@gmail.com>
2021-03-10 12:28:28 +0100

    logs sent correctly formated

--- api/routes/loggs.js
+++ api/routes/loggs.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 const Logg = require('../models/logg');
 const moment = require('moment');
 
-const today = moment().startOf('day');
+var today = moment().startOf('day');
 
 
 router.get('/', (req, res, next) =>{
@@ -33,9 +33,11 @@ router.get('/:userId', (req, res, next) =>{
     });
 });
 
-router.get('/:userId/:date', (req, res, next) =>{
+router.get('/:userId/:days', (req, res, next) =>{
+    var today = moment().startOf('day');
     console.log("ee");
     const id = req.params.userId;
+    const days =req.params.days;
     Logg.aggregate(
         [
             { $match : { userid : id }},
@@ -51,7 +53,41 @@ router.get('/:userId/:date', (req, res, next) =>{
       )
     .exec()
     .then(docs => {
-        res.status(200).json(docs);
+        jsonObj = [];
+        realDays = [];
+        var new_date = moment(docs[0]._id);
+        var count = 0;
+        for(var i = 0; i<docs.length;i++) {
+            realDays.push(today.diff(moment(docs[i]._id), 'days'));
+            console.log(moment(docs[i]._id));
+            console.log(today.diff(moment(docs[i]._id), 'days'));
+        }
+        realDays.sort();
+        for(var i = 0; i<days;i++) {
+            new_date = moment(today, "DD-MM-YYYY").subtract(i-1, 'days');
+            if(count<realDays.length){
+                if(i == realDays[count]){
+                    
+                    jsonObj.push({
+                        'id': new_date,
+                        'water': docs[count].WaterSum
+                    });
+                    count = count + 1;
+
+                } else{
+                    jsonObj.push({
+                        'id': new_date,
+                        'water': 0
+                    });
+                }
+            }else{
+                jsonObj.push({
+                    'id': new_date,
+                    'water': 0
+                });
+            } 
+        }
+        res.status(200).json(jsonObj);
     })
     .catch(err => {
     console.log(err);
